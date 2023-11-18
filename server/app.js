39cdd438d3ef2922baad527d78cd575b34a5d15b
nawab55 <mdnawab3674@gmail.com>
2023-11-18 13:14:50 +0530

    express middleware example

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
-const http = require("http");
 
-const routes = require('./routes');
-console.log(routes.someText);
+const express = require('express');
+const app = express();
+// const routes = require('./routes');
+// console.log(routes.someText);
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next();
+});
+app.use((req, res, next) => {
+    console.log("In another middleware!");
+    res.send('<h1>Hello from express</h1>')
+    // next();
+});
 
-const server = http.createServer(routes.handler);
 
-server.listen(3000);
+app.listen(4000);
