bf614d411c23c9e953016bad2c06b4d5dbb10465
nawab55 <mdnawab3674@gmail.com>
2023-12-24 12:25:08 +0530

    Retrieving data from databases and When i click on delete button the product should get deleted.

---
+++ RetrieveDataFromDatabaseWithDeleteFunctionality/util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node_complete',
+    password: 'Nawab@#$123'
+});
+
+module.exports = pool.promise();
